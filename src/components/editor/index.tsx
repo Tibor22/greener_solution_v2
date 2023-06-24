d9b38cc31d61b15adfa7afc37fc6d426eea68c5b
Tibor22 <tothtibitravel@gmail.com>
2023-06-24 13:43:02 +0100

    working on tiptap

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -19,6 +19,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 import { fonts } from '@/styles/common';
 import { merriweather } from '@/styles/fonts';
+import HardBreak from '@tiptap/extension-hard-break';
 // import SEOForm, { SeoResult } from './SEOForm';
 // import ActionButton from '../common/ActionButton';
 // import ThumbnailSelector from './ThumbnailSelector';
@@ -78,6 +79,14 @@ const Editor: FC<Props> = ({
 			Placeholder.configure({
 				placeholder: 'Type something',
 			}),
+			HardBreak.extend({
+				addKeyboardShortcuts() {
+					return {
+						ArrowDown: () => this.editor.commands.setHardBreak(),
+					};
+				},
+			}),
+
 			// Youtube.configure({
 			// 	width: 840,
 			// 	height: 472.5,
@@ -100,12 +109,15 @@ const Editor: FC<Props> = ({
 				);
 				if (selectionRange) setSelectionRange(selectionRange);
 			},
+
 			// attributes: {
 			// 	class:
 			// 		'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full',
 			// },
 		},
 	});
+
+	console.log('EDITOR:', editor);
 
 	const handleImageSelection = (result: ImageSelectionResult) => {
 		editor
@@ -178,7 +190,7 @@ const Editor: FC<Props> = ({
 					{/* SPACER */}
 				</EditorNav>
 				{/* {editor ? <EditLink editor={editor} /> : null} */}
-				<EditorContent editor={editor} />
+				<CEditorContent editor={editor} />
 				{/* SPACER */}
 				{/* <SEOForm
 					onChange={updateSEOValue}
@@ -200,6 +212,25 @@ const Editor: FC<Props> = ({
 	);
 };
 
+const CEditorContent = styled(EditorContent)<{ editor: any }>`
+	margin-top: 1rem;
+	& .ProseMirror {
+		min-height: 300px;
+		font-size: ${fonts.regular};
+		max-width: 100%;
+		padding-right: 0.75rem;
+		color: black;
+	}
+
+	& .ProseMirror p.is-editor-empty:first-child::before {
+		color: #adb5bd;
+		content: attr(data-placeholder);
+		float: left;
+		height: 0;
+		pointer-events: none;
+	}
+`;
+
 const Title = styled.input`
 	width: 100%;
 	margin-top: 2rem;
@@ -220,3 +251,211 @@ const EditorContainer = styled.div`
 
 const EditorNav = styled.div``;
 export default Editor;
+
+// import { Color } from '@tiptap/extension-color';
+// import ListItem from '@tiptap/extension-list-item';
+// import TextStyle from '@tiptap/extension-text-style';
+// import { EditorContent, useEditor } from '@tiptap/react';
+// import StarterKit from '@tiptap/starter-kit';
+// import React from 'react';
+
+// export const Editor = () => {
+// 	const editor = useEditor({
+// 		extensions: [
+// 			StarterKit,
+// 			Underline,
+// 			Link.configure({
+// 				autolink: false,
+// 				linkOnPaste: false,
+// 				openOnClick: false,
+// 				HTMLAttributes: {
+// 					target: '',
+// 				},
+// 			}),
+// 			Placeholder.configure({
+// 				placeholder: 'Type something',
+// 			}),
+// 			HardBreak.extend({
+// 				addKeyboardShortcuts() {
+// 					return {
+// 						ArrowDown: () => this.editor.commands.setHardBreak(),
+// 					};
+// 				},
+// 			}),
+
+// 			// Youtube.configure({
+// 			// 	width: 840,
+// 			// 	height: 472.5,
+// 			// 	HTMLAttributes: {
+// 			// 		class: 'mx-auto rounded',
+// 			// 	},
+// 			// }),
+// 			// TipTapImage.configure({
+// 			// 	HTMLAttributes: {
+// 			// 		class: 'mx-auto',
+// 			// 	},
+// 			// }),
+// 		],
+// 		// editorProps: {
+// 		// 	handleClick(view, pos, event) {
+// 		// 		const { state } = view;
+// 		// 		const selectionRange = getMarkRange(
+// 		// 			state.doc.resolve(pos),
+// 		// 			state.schema.marks.link
+// 		// 		);
+// 		// 		if (selectionRange) setSelectionRange(selectionRange);
+// 		// 	},
+
+// 		// attributes: {
+// 		// 	class:
+// 		// 		'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full',
+// 		// },
+// 		// },
+// 	});
+
+// 	console.log('EDITOR INSIDE EDITOR:', editor);
+
+// 	return (
+// 		<div>
+// 			<MenuBar editor={editor} />
+// 			<EditorContent editor={editor} />
+// 		</div>
+// 	);
+// };
+
+// const MenuBar = (editor: any) => {
+// 	if (!editor) {
+// 		return null;
+// 	}
+// 	console.log('EDITOR:', editor);
+// 	return (
+// 		<>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleBold().run()}
+// 				disabled={!editor.can().chain().focus().toggleBold().run()}
+// 				className={editor.isActive('bold') ? 'is-active' : ''}
+// 			>
+// 				bold
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleItalic().run()}
+// 				disabled={!editor.can().chain().focus().toggleItalic().run()}
+// 				className={editor.isActive('italic') ? 'is-active' : ''}
+// 			>
+// 				italic
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleStrike().run()}
+// 				disabled={!editor.can().chain().focus().toggleStrike().run()}
+// 				className={editor.isActive('strike') ? 'is-active' : ''}
+// 			>
+// 				strike
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleCode().run()}
+// 				disabled={!editor.can().chain().focus().toggleCode().run()}
+// 				className={editor.isActive('code') ? 'is-active' : ''}
+// 			>
+// 				code
+// 			</button>
+// 			<button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+// 				clear marks
+// 			</button>
+// 			<button onClick={() => editor.chain().focus().clearNodes().run()}>
+// 				clear nodes
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().setParagraph().run()}
+// 				className={editor.isActive('paragraph') ? 'is-active' : ''}
+// 			>
+// 				paragraph
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+// 				className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+// 			>
+// 				h1
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+// 				className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+// 			>
+// 				h2
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+// 				className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+// 			>
+// 				h3
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+// 				className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+// 			>
+// 				h4
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+// 				className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+// 			>
+// 				h5
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+// 				className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+// 			>
+// 				h6
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleBulletList().run()}
+// 				className={editor.isActive('bulletList') ? 'is-active' : ''}
+// 			>
+// 				bullet list
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleOrderedList().run()}
+// 				className={editor.isActive('orderedList') ? 'is-active' : ''}
+// 			>
+// 				ordered list
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+// 				className={editor.isActive('codeBlock') ? 'is-active' : ''}
+// 			>
+// 				code block
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().toggleBlockquote().run()}
+// 				className={editor.isActive('blockquote') ? 'is-active' : ''}
+// 			>
+// 				blockquote
+// 			</button>
+// 			<button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+// 				horizontal rule
+// 			</button>
+// 			<button onClick={() => editor.chain().focus().setHardBreak().run()}>
+// 				hard break
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().undo().run()}
+// 				disabled={!editor.can().chain().focus().undo().run()}
+// 			>
+// 				undo
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().redo().run()}
+// 				disabled={!editor.can().chain().focus().redo().run()}
+// 			>
+// 				redo
+// 			</button>
+// 			<button
+// 				onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+// 				className={
+// 					editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''
+// 				}
+// 			>
+// 				purple
+// 			</button>
+// 		</>
+// 	);
+// };
