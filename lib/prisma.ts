8e9bbd804f4689087e0f71186b24af9a0eac3e47
Tibor22 <tothtibitravel@gmail.com>
2023-09-19 12:57:03 +0100

    p

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,35 +1,11 @@
-// import { PrismaClient } from '@prisma/client';
-
-// const globalForPrisma = globalThis as unknown as {
-// 	prisma: PrismaClient | undefined;
-// };
-
-// const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// export default prisma;
-
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-	namespace NodeJS {
-		interface Global {
-			prisma: PrismaClient;
-		}
-	}
-}
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClient | undefined;
+};
 
-let prisma: PrismaClient;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
-} else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
-
-	prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
