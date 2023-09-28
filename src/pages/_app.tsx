41255c78fb3d3569a93a070d32257e30ed5a55cf
Tibor22 <tothtibitravel@gmail.com>
2023-09-28 12:58:38 +0100

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import ThemeProvider from '@/providers/ThemeProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({
 	Component,
@@ -9,6 +10,7 @@ export default function App({
 	return (
 		<SessionProvider session={session}>
 			<ThemeProvider>
+				<Analytics />
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</SessionProvider>
